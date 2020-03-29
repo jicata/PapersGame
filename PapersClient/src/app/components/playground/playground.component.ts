92be60f7241faafcf02dfc52fdbbd72d4ea23960
jicata <jitza_nest@yahoo.com>
2020-03-29 21:27:18 +0300

    Added PlayGround Component

--- PapersClient/src/app/components/playground/playground.component.ts
+++ PapersClient/src/app/components/playground/playground.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, Input } from "@angular/core";
+import { HubConnection } from "@microsoft/signalr";
+import { PlaygroundService } from "src/app/services/signalr/playground.service";
 
 @Component({
-  selector: 'app-playground',
-  templateUrl: './playground.component.html',
-  styleUrls: ['./playground.component.scss']
+  selector: "app-playground",
+  templateUrl: "./playground.component.html",
+  styleUrls: ["./playground.component.scss"]
 })
 export class PlaygroundComponent implements OnInit {
+  hubConnection: HubConnection;
+  players: string[] = [];
 
-  constructor() { }
+  constructor(private playgroundSignalRService: PlaygroundService) {}
 
   ngOnInit(): void {
+    this.playgroundSignalRService
+      .subscribeToBroadCastGameSession(this.addUser.bind(this));
   }
 
+  addUser = (msg) => {
+    console.log(msg)
+    this.players = msg;
+  }
+
+  onLoadNext() {
+    console.log(this.players);
+  }
 }
