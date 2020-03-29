79f2bd89ba32ed17713d3abd96a05b8f8e04fbd7
jicata <jitza_nest@yahoo.com>
2020-03-29 21:27:34 +0300

    Routing

--- PapersClient/src/app/app-routing.module.ts
+++ PapersClient/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreatePapersComponent } from './components/create-papers/create-papers.component';
+import { PlaygroundComponent } from './components/playground/playground.component';
 
 const routes: Routes = [
   { path: '', component: CreatePapersComponent, pathMatch: 'full' },
+  { path: 'playground', component: PlaygroundComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
--- PapersClient/src/app/app.module.ts
+++ PapersClient/src/app/app.module.ts
@@ -6,16 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaperComponent } from './components/paper/paper.component';
 import { CreatePapersComponent } from './components/create-papers/create-papers.component';
-import { CreatePapersService } from './services/create-papers.service';
+import { CreatePapersService } from './services/create-papers/create-papers.service';
 import { HttpClientModule } from '@angular/common/http';
 
 import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
+import { PlaygroundComponent } from './components/playground/playground.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaperComponent,
-    CreatePapersComponent
+    CreatePapersComponent,
+    PlaygroundComponent
   ],
   imports: [
     HttpClientModule,
